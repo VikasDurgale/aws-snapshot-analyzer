e829ed6c0e0e40e9c59d4a64d01ed2fd80802586
admin <vdurgale@tibco.com>
2019-03-03 02:17:52 +0530

    Wait for instances to stop and start during snapshot process

--- snapshot/snapshot.py
+++ snapshot/snapshot.py
@@ -83,10 +83,22 @@ def create_snapshots(project):
 	instances = filter_instances(project)
 
 	for i in instances:
+		print("Stopping {0}...".format(i.id))
+
+		i.stop()
+		i.wait_until_stopped()
+
 		for v in i.volumes.all():
-			print("Creating snapshot of {0}...".format(v.id))
+			print(" Creating snapshot of {0}...".format(v.id))
 			v.create_snapshot(Description="Created by AWS-SnapshotAnalyzer")
 
+		print("Starting {0}...".format(i.id))
+
+		i.start()
+		i.wait_until_running()
+
+	print("Job finished!")
+	
 	return
 
 @instances.command('list')
